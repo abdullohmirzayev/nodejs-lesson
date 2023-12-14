8c7ad8e631fdd7a2c6250cfdaf8b9001fb1266fb
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-14 20:40:56 +0500

    added web-server

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const http = require("http");
-const server = http.createServer((request, response) => {
-  // request - serverga so'rov
-  // response - server javobi
-  console.log(request.url);
-
-  response.write("<h1>Hello new server</h1>");
-  response.end();
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "content-Type": "text/html" });
+    res.end(`
+      <h2>Sent name</h2>
+      <form method='post' action='/'>
+        <input name='name' placeholder='Enter your email' />
+        <button type='submit'>Sent name</button>
+      </form>
+    `);
+  } else if (req.method === "POST") {
+    const body = [];
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+      res.end(`Email successfully added: ${message}`);
+    });
+  }
 });
 server.listen(3000, () => {
-  console.log("Server has been started on port: 3000");
+  console.log("Name has been started on port: 3000");
 });
