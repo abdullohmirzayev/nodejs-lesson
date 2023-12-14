f98a28f60d97af4a7ff0af0cbddacbb23a2b670a
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-14 23:10:33 +0500

    added #14. HTML bilan ishlash

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
-    res.writeHead(200, { "content-Type": "text/html" });
-    res.end(`
-      <h2>Sent name</h2>
-      <form method='post' action='/'>
-        <input name='name' placeholder='Enter your email' />
-        <button type='submit'>Sent name</button>
-      </form>
-    `);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "templates", "index.html"),
+        "utf-8",
+        (err, conten) => {
+          if (err) throw err;
+          res.end(conten);
+        }
+      );
+    } else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "templates", "about.html"),
+        "utf-8",
+        (err, conten) => {
+          if (err) throw err;
+          res.end(conten);
+        }
+      );
+    } else if (req.url === "/contact") {
+      fs.readFile(
+        path.join(__dirname, "templates", "contact.html"),
+        "utf-8",
+        (err, conten) => {
+          if (err) throw err;
+          res.end(conten);
+        }
+      );
+    }
   } else if (req.method === "POST") {
     const body = [];
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     req.on("data", (data) => {
       body.push(Buffer.from(data));
     });
     req.on("end", () => {
       const message = body.toString().split("=")[1];
-      res.end(`Email successfully added: ${message}`);
+      res.end(`Name successfully added: ${message}`);
     });
   }
 });
 server.listen(3000, () => {
-  console.log("Name has been started on port: 3000");
+  console.log("Server has been started on port: 3000");
 });
